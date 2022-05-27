3b8c7fbda3110afcd3dcc6bbec8aacf7bc329103
kingwolf_book14 <lccr777@163.com>
2022-05-28 05:41:22 +0800

    testcase设置allure报告格式

--- pageobject/case/test_baidutieba_login.py
+++ pageobject/case/test_baidutieba_login.py
@@ -3,13 +3,19 @@
 # createtime : 2022/4/14 23:34
 
 import pytest
+import allure
 from ..page.login_page import LoginPage
 
+@allure.feature("测试百度贴吧")
 class TestTiebaLogin(object):
-
+    
+    @allure.story("测试百度贴吧自动登录以及自动签到功能")
+    @allure.dynamic.title("默认账号自动登录百度贴吧，并且完成自动签到所以贴吧")
     def test_login(self):
-        d = LoginPage(browser="chrome")
-        d.go_sign("1069645896@qq.com", "922521dfxs5619")
+        with allure.step("登录处理"):
+            d = LoginPage(browser="chrome")
+        with allure.step("签到处理"):
+            d.go_sign("1069645896@qq.com", "922521dfxs5619")
 
 
 if __name__ == '__main__':
