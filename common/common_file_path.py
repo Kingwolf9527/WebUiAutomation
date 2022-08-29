cbe1a9f3aa2f1e63c86e9c382343d2c576c2b7cb
kingwolf_book14 <lccr777@163.com>
2022-08-29 19:05:26 +0800

    新增pathlib库来替代os模块，处理路径相关问题

--- common/common_file_path.py
+++ common/common_file_path.py
@@ -5,48 +5,34 @@
 import sys
 sys.path.append(".")
 sys.path.append(r"F:\WebUiAutomation")
-import os
+from pathlib import Path
 
 
-# # 谷歌浏览器驱动
-# chrome_driver_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__name__))),
-#                                    "data", "browser_drivers", "chromedriver.exe")
-# # 火狐浏览器驱动
-# firefox_driver_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__name__))),
-#                                    "data", "browser_drivers", "geckodriver.exe")
-# # edge浏览器驱动
-# edge_driver_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__name__))),
-#                                 "data", "browser_drivers", "msedgedriver.exe")
 
 # 处理allure报告的路径
-allure_json_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'allure_report', 'json_path')
-
-allure_html_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'allure_report', 'allure_path')
+allure_json_path = Path.cwd().parent.joinpath("allure_report", "json_path")
+allure_html_path = Path.cwd().parent.joinpath("allure_report", "allure_path")
 
 
 # 基础yaml配置路径
-base_yaml_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data")
+base_yaml_path = Path.cwd().parent.joinpath("data")
+# base_yaml_path = Path.cwd().parent / 'data'
 
 # 配置文件路径
-base_config_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "base.yaml")
+base_config_path = Path.cwd().parent.joinpath("data", "base.yaml")
 
 # 数据库yaml格式配置文件
-db_config = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "config", "db_config.yaml")
+db_config = Path.cwd().parent.joinpath("config", "db_config.yaml")
 
 # 普通用户账号数据
-foreground_infos = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data", "foreground.yaml")
-
+foreground_infos = Path.cwd().parent.joinpath("data", "foreground.yaml")
 
 # 截图保存文件路径
-image_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "images")
-
+image_dir = Path.cwd().parent.joinpath("images")
 
 # 默认Excel文件的路径
-default_excel_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-                                  "data", "excel_data.xlsx")
+default_excel_path = Path.cwd().parent.joinpath("data", "excel_data.xlsx")
 
 # 默认sheet_name
 default_sheet_name = "users"
-
-print(base_yaml_path)
 
