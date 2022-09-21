5456037b72c0fefc5975766ae120c8f8c3f5850e
kingwolf_book14 <lccr777@163.com>
2022-09-21 17:10:36 +0800

    新增了bvt冒烟测试以及对应yaml文件

--- pageobject/page/bvt_test_page.py
+++ pageobject/page/bvt_test_page.py
@@ -8,77 +8,49 @@ import sys
 sys.path.append(".")
 sys.path.append(r"F:\WebUiAutomation")
 import time
-from selenium.webdriver.common.by import By
-from pageobject.page.tpshop_login_page import TpshopLoginPage
+from pageobject.base.base_handle import BaseHandle
 from common.common_log import CommonLog
 
 logger = CommonLog().get_logger()
 
 
-class Bvt(TpshopLoginPage):
+class Bvt(BaseHandle):
     
-    search_box = (By.XPATH, '//input[@class="search_usercenter_text"]')
-    search_btn = (By.XPATH, '//a[@class="search_usercenter_btn"]')
-    search_text = "欧式陶瓷"
-    add_to_cart = (By.XPATH, '//div[@class="p-btn"]/a')
-    join_cart = (By.XPATH, '//a[@class="addcar buy_button"]')
-    # 嵌套iframe
-    go_settlement_iframe = (By.XPATH, '//div[@class="layui-layer-content"]/iframe')
-    go_settlement = (By.XPATH, '//a[contains(@class, "ui-button-122")]')
-    # 购物车页面结算
-    pay_total = (By.XPATH, '//a[@class="paytotal"]')
-    # 提前获取商品总金额(例如：￥0.02)--处理数字
-    total_fee = (By.XPATH, '//div[contains(@class,"column t-sum sumpri")]/span')
-    # 这一步处理test环境支付弹窗
-    # 勾选使用账户余额
-    use_balance = (By.XPATH, '//input[@id="user_money_checkbox"]')
-    # 输入余额
-    input_balance = (By.XPATH, '//input[@id="user_money"]')
-    # 支付密码(6666aaaa)
-    pay_pwd = (By.XPATH, '//p[@class="item"][4]//input')
-    # 提交订单
-    summit_order = (By.XPATH, '//button[@id="submit_order"]')
-    # 代发货元素
-    deliver = (By.XPATH, '//h1[@class="ddn2"]')
     
-    def bvt_test(self, username, password, verify_code):
+    def bvt_test(self, **kwargs):
         """
-        
-        @param username:
-        @param password:
-        @param verify_code:
+        bvt测试page页面
+        @param kwargs:
         @return:
         """
-        # 先登錄
-        self.tpshop_login(username, password, verify_code)
         # 再做其他操作
-        self.input(Bvt.search_box, Bvt.search_text)
-        self.click(Bvt.search_btn)
-        self.click(Bvt.add_to_cart)
-        self.click(Bvt.join_cart)
+        self.input(kwargs["bvt_base"]["search_box"], kwargs["bvt_base"]["search_text"])
+        self.click(kwargs["bvt_base"]["search_btn"])
+        self.click(kwargs["bvt_base"]["add_to_cart"])
+        self.click(kwargs["bvt_base"]["join_cart"])
         # 切换iframe
-        self.iframe_into(Bvt.go_settlement_iframe)
-        self.click(Bvt.go_settlement)
+        self.iframe_into(kwargs["bvt_base"]["go_settlement_iframe"])
+        self.click(kwargs["bvt_base"]["go_settlement"])
         # 退出iframe
         self.iframe_out()
         time.sleep(2)
         # 购物车页面以及获取总金额
-        total_money = self.get_element_text(Bvt.total_fee)
+        total_money = self.get_element_text(kwargs["bvt_base"]["total_fee"])
         # 字符串转为浮点数
         pay_num = float(total_money.split("￥")[-1])
-        self.click(Bvt.pay_total)
-        # 处理alert弹窗
-        self.switchto_alert()
+        self.click(kwargs["bvt_base"]["pay_total"])
+        # # 处理alert弹窗
+        # self.switchto_alert()
         time.sleep(7)
-        # 输入积分
-        self.click(Bvt.use_balance)
+        # 输入账户余额
+        self.click(kwargs["bvt_base"]["use_balance"])
         time.sleep(1)
-        self.input(Bvt.input_balance, pay_num)
+        self.input(kwargs["bvt_base"]["input_balance"], pay_num)
         time.sleep(2)
         # 输入支付密码
-        self.input(Bvt.pay_pwd, "6666aaaa")
+        self.input(kwargs["bvt_base"]["pay_pwd_ele"], kwargs["bvt_base"]["pay_pwd"])
         # 提交订单
-        self.click(Bvt.summit_order)
+        self.click(kwargs["bvt_base"]["summit_order"])
         time.sleep(3)
 
 
--- testcase/test_bvt.py
+++ testcase/test_bvt.py
@@ -10,8 +10,9 @@ sys.path.append(r"F:\WebUiAutomation")
 import pytest
 import allure
 from pageobject.page.bvt_test_page import Bvt
+from pageobject.page.tpshop_login_page import TpshopLoginPage
 from common.common_config_yaml import CommonYaml
-from common.common_file_path import foreground_infos
+from common.common_file_path import  base_yaml_path
 from common.common_log import CommonLog
 
 logger = CommonLog().get_logger()
@@ -20,19 +21,30 @@ logger = CommonLog().get_logger()
 class TestBvt(object):
     
     @allure.story("登录-搜索产品-添加购物车-购物车结算-使用余额支付-提交订单")
-    @pytest.mark.parametrize("infos", CommonYaml(foreground_infos).read_yaml())
-    def test_bvt(self, infos, get_driver):
+    @pytest.mark.parametrize("account_info", CommonYaml(base_yaml_path / 'login_data.yaml').read_yaml())
+    @pytest.mark.parametrize("bvt_infos", CommonYaml(base_yaml_path.joinpath('bvt.yaml')).read_yaml())
+    def test_bvt(self, account_info, bvt_infos, get_driver):
         """
-        新增地址
-        @param infos:
+        BVT测试
+        @param account_info:
+        @param bvt_infos:
         @param get_driver:
         @return:
         """
-        logger.info(f"--------正在执行冒烟試操作！ --------")
+        logger.info(f"--------正在执行冒烟测试操作！ --------")
+        
+        logger.info(f"--------正在执行登录操作！ --------")
+        d = TpshopLoginPage(get_driver)
+        d.tpshop_login(**account_info)
+        assert d.is_displayed(account_info['login_base']['exchanges']) == True
+        logger.info(f"--------登录成功！ --------")
+        
+        logger.info(f"-------- 正在执行搜索产品-添加购物车-购物车结算-使用余额支付-提交订单测试！--------")
         dd = Bvt(get_driver)
-        dd.bvt_test(username=infos["username"], password=infos["password"], verify_code="2222")
-        assert dd.is_displayed(dd.deliver) is True
-        logger.info(f"-------- 冒烟测试成功！！ --------")
+        dd.bvt_test(**bvt_infos)
+        assert dd.is_displayed(bvt_infos["bvt_base"]["deliver"]) == True
+        logger.info(f'-------- 当前页面的‘待发货’元素是否显示：{dd.is_displayed(bvt_infos["bvt_base"]["deliver"])} --------')
+        logger.info(f"-------- 冒烟测试成功！ --------")
 
 if __name__ == '__main__':
     pytest.main()
